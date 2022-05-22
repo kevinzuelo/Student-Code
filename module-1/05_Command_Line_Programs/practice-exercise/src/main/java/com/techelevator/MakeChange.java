c49a92ee83bfa6f89ad917d6cfd6cc72d666f5ef
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-22 10:34:36 -0400

    Completed Command Line practice exercises

--- module-1/05_Command_Line_Programs/practice-exercise/src/main/java/com/techelevator/MakeChange.java
+++ module-1/05_Command_Line_Programs/practice-exercise/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,7 +14,14 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Please enter the amount of the bill: ");
+		double billAmount = Double.parseDouble(scanner.nextLine());
+		System.out.println("Please enter the amount tendered: ");
+		double amountTendered = Double.parseDouble(scanner.nextLine());
+		System.out.println("The change required is " + (amountTendered - billAmount));
+		scanner.close();
 	}
 
 }
--- module-1/05_Command_Line_Programs/practice-exercise/src/main/java/com/techelevator/MartianWeight.java
+++ module-1/05_Command_Line_Programs/practice-exercise/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
 In case you've ever pondered how much you weigh on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,7 +21,19 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String userWeightsInput = "";
+
+		System.out.println("Enter a series of Earth weights (space-separated): ");
+		userWeightsInput = scanner.nextLine();
+
+		String[] splitString = userWeightsInput.split( " ");
+
+		for (int i = 0; i < splitString.length; i++) {
+			System.out.println(splitString[i] + " lbs. on Earth is " + (int)(Integer.parseInt(splitString[i]) * 0.378 ) + " lbs. on Mars." );
+		}
+		scanner.close();
 	}
 
 }
