bfda9281c9b11066cb1b94daec6e73a659fa230b
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-23 19:36:28 -0400

    Intro to Objects example

---
+++ module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Album.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+public class Album {
+    // Properties
+    String albumName;
+    String[] artists;
+    String[] trackList;
+    int releaseYear;
+    String genre;
+
+    // Constructors
+    Album(String albumName, String[] artists, String[] trackList, int releaseYear, String genre) {
+        this.albumName = albumName;
+        this.artists = artists;
+        this.trackList = trackList;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    // Methods
+}
