a77684f7b7c450f8a6b620fcce7ddd6c0d47aee9
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-22 22:25:01 -0400

    Added a custom method and felt better

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 
 public class DecimalToBinary {
 
+	public static String convertToBinary(int userDecimal) {
+		int[] binaryArray = new int[500];
+		int index = 0;
+		String binaryNumber = "";
+
+		while (userDecimal > 0) {
+			binaryArray[index] = userDecimal % 2;
+			userDecimal = userDecimal / 2;
+			index++;
+		}
+		// Print array backwards
+		//for (start at the last element; until we're back at 0; count down)
+		for (int i = index - 1; i >= 0; i--) {
+			binaryNumber += binaryArray[i];
+		}
+		return binaryNumber;
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int[] parsedToInts;
@@ -20,11 +38,15 @@ public class DecimalToBinary {
 			parsedToInts[i] = Integer.parseInt(splitString[i]);
 		}
 
-		// One way to do it
-		for (int num : parsedToInts) {
-			System.out.println(num + " in binary is " + Integer.toBinaryString(num));
-		}
+//		// One (easy) way to do it
+//		for (int num : parsedToInts) {
+//			System.out.println(num + " in binary is " + Integer.toBinaryString(num));
+//		}
 
+		// Another way to do it (using the custom method)
+		for (int num : parsedToInts) {
+			System.out.println(num + " in binary is " + convertToBinary(num));
+		}
 	scanner.close();
 	}
 }
