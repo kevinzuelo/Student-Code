be2f572e010365f76f2e04648111e77a9385bf21
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-24 11:30:50 -0400

    Followed Collections lecture

--- module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,8 +1,6 @@
 package com.techelevator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lecture {
 
@@ -11,36 +9,65 @@ public class Lecture {
 		System.out.println("       LISTS");
 		System.out.println("####################");
 
+		List<String> dogsBreeds = new ArrayList<>();
+		dogsBreeds.add("Pomeranian");
+		dogsBreeds.add("Dachsund");
+		dogsBreeds.add("Greyhound");
+		dogsBreeds.add("Golden Retriever");
 
 		System.out.println("####################");
 		System.out.println("Lists are ordered");
 		System.out.println("####################");
 
+		for (String names : dogsBreeds) {
+			System.out.println(names);
+		}
+
+//		for (int i = 0; i < dogsBreeds.size(); i++) {
+//			System.out.println(dogsBreeds.get(i));
+//		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow duplicates");
 		System.out.println("####################");
 
+		dogsBreeds.add("Pomeranian");
+
+		for (String names : dogsBreeds) {
+			System.out.println(names);
+		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be inserted in the middle");
 		System.out.println("####################");
 
+		dogsBreeds.add(2,"Dalmatian");
+
+		for (String names : dogsBreeds) {
+			System.out.println(names);
+		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be removed by index");
 		System.out.println("####################");
 
+		dogsBreeds.remove(2);
+
+		for (String names : dogsBreeds) {
+			System.out.println(names);
+		}
 
 		System.out.println("####################");
 		System.out.println("Find out if something is already in the List");
 		System.out.println("####################");
 
+		if(dogsBreeds.contains("Greyhound")) {
+			System.out.println("This list does contain: " + "Greyhound" );
+		}
 
 		System.out.println("####################");
 		System.out.println("Find index of item in List");
 		System.out.println("####################");
-
 
 		System.out.println("####################");
 		System.out.println("Lists can be turned into an array");
@@ -66,6 +93,7 @@ public class Lecture {
 		System.out.println("       STACKS");
 		System.out.println("####################");
 
+		Stack<String> myNewStack = new Stack<String>();
 
 		System.out.println("####################");
 		System.out.println("Stacks are ordered, allow duplicates");
@@ -90,6 +118,7 @@ public class Lecture {
 		System.out.println("       QUEUES");
 		System.out.println("####################");
 
+		Queue<String> myNewQueue = new LinkedList<>();
 
 		System.out.println("####################");
 		System.out.println("Queues are ordered, allow duplicates");
