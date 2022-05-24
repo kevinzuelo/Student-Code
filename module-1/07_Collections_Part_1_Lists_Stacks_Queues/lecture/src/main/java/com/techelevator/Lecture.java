88ff5eb7809108a300c05221096b3f9a84b351d9
Alex <ben>
2022-05-24 08:50:00 -0400

    Lecture for Day 7) Collections Part 1, Tutorials for Day 8

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,109 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		System.out.println("####################");
+		System.out.println("       LISTS");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists are ordered");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow duplicates");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be inserted in the middle");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be removed by index");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find out if something is already in the List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find index of item in List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be turned into an array");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be sorted");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be reversed too");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists work with for / for-each");
+		System.out.println("####################");
+		System.out.println();
+
+		System.out.println("####################");
+		System.out.println("       STACKS");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Stacks are ordered, allow duplicates");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Stacks allow duplicates");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Stacks are LIFO");
+		System.out.println("####################");
+
+		System.out.println("####################");
+		System.out.println("Stacks work with for-each for reading");
+		System.out.println("####################");
+		System.out.println();
+
+		System.out.println("####################");
+		System.out.println("       QUEUES");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Queues are ordered, allow duplicates");
+		System.out.println("####################");
+
+		System.out.println("####################");
+		System.out.println("Queues are FIFO");
+		System.out.println("####################");
+
+		System.out.println("####################");
+		System.out.println("Queues work with for-each for reading");
+		System.out.println("####################");
+		System.out.println();
+
+
+	}
+}
