6381659d1108210a63103aaf97bfcb606aef800a
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-25 11:23:36 -0400

    Followed Collections pt2 tutorial

--- module-1/08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -10,11 +10,23 @@ public class Tutorial {
 
         // Step One: Declare a Map
 
+        Map<String, String> projects;
+        projects = new HashMap<>();
     	
     	// Step Two: Add items to a Map
 
+        projects.put("Ada", "Inventing Computer Programming");
+        projects.put("Grace", "COBOL");
+        projects.put("Margaret", "Apollo Guidance Computer");
+        projects.put("Adele", "Graphical User Interfaces");
     	
     	// Step Three: Loop through a Map
+
+        projects.remove("Grace");
+
+        for(Map.Entry<String, String> entries : projects.entrySet()) {
+            System.out.println("The key " + entries.getKey() + " is linked to the value " + entries.getValue());
+        }
 
     }
 
