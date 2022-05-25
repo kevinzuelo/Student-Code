6a04a4e543824112f051edeb67003058f14fe36d
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-25 11:22:46 -0400

    Followed Collections pt2 lecture

--- module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
@@ -13,7 +13,24 @@ public class Lecture {
 		System.out.println("####################");
 		System.out.println();
 
+		Map<String, Integer> populations = new HashMap<>();
 
+		populations.put("NY", 20201249);
+		populations.put("PA", 13002700);
+		populations.put("MA", 7029917);
+		populations.put("OH", 11799447);
+
+		System.out.println("Populations included: " + populations.size());
+
+		System.out.println("The population of NY is: " + populations.get("NY"));
+
+		for (String state : populations.keySet()) {
+			System.out.println("The population of " + state + " is " + populations.get(state));
+		}
+
+		for(Map.Entry<String, Integer> population : populations.entrySet()) {
+			System.out.println("The population of " + population.getKey());
+		}
 	}
 
 }
