3be16365a7fb7b0df0c43ed2f9b9b7203cc1820a
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-05-26 08:50:23 -0400

    Completed Classes and Encapsulation tutorial

--- module-1/09_Classes_and_Encapsulation/tutorial/src/main/java/com/techelevator/Bookstore.java
+++ module-1/09_Classes_and_Encapsulation/tutorial/src/main/java/com/techelevator/Bookstore.java
@@ -11,10 +11,20 @@ public class Bookstore {
         System.out.println();
 
         // Step Three: Test the getters and setters
+        Book twoCities = new Book();
+        twoCities.setTitle("A Tale of Two Cities");
+        twoCities.setAuthor("Charles Dickens");
+        twoCities.setPrice(14.99);
+        System.out.println(twoCities.bookInfoToString() + "\n");
 
         // Step Five: Test the Book constructor
+        Book blooKitties = new Book("A Pail of Blue Kitties", "Gnarls Lickens", 19.99);
+        System.out.println(blooKitties.bookInfoToString());
 
         // Step Nine: Test the ShoppingCart class
-        
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.add(twoCities);
+        shoppingCart.add(blooKitties);
+        System.out.println(shoppingCart.receipt());
     }
 }
