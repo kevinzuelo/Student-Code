4bff12cc48847b299314809bd6ab36971c63fa6e
Alex <ben>
2022-05-30 08:55:13 -0400

    Lecture-final and card-example-final from Day 10 review, plus tutorials for Day 11, Inheritance

---
+++ module-1/10_Review_Day/card-example-final/src/com/games/Game.java
@@ -0,0 +1,29 @@
+package com.games;
+
+public class Game {
+    // Properties
+    private Deck deck;
+
+
+
+    // Constructors
+    public Game() {
+        deck = new Deck();
+    }
+
+    // getters and setters
+    public Deck getDeck() {
+        return deck;
+    }
+
+//    public void setDeck(Deck deck) {
+// //        if (user.permissions == "MANAGER" && deck != null)
+//        this.deck = deck;
+//    }
+
+
+    // other methods
+
+
+
+}
