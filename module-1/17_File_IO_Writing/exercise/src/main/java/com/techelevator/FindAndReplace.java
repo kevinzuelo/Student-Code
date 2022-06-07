fb2ae36f23e35f72ce4349b38ff20e9bc55441a7
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-06-07 16:11:38 -0400

    Fixed Exceptions

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class FindAndReplace {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("What is the search word?");
@@ -41,7 +41,6 @@ public class FindAndReplace {
         }
         catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
-            throw e;
         }
         userInput.close();
     }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class FizzWriter {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 
 		System.out.print("Please enter the destination path: ");
