e311e2e360058d11beef2deb63ca56d76f806837
Ben Langhinrichs <benjamin@techelevator.com>
2022-07-28 11:43:22 -0400

    Exercises and lecture-final for M3-Day 13, Vue Component Communication

---
+++ module-3/13_Vue_Component_Communication/lecture-final/src/components/AverageSummary.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="well">
+    <span class="amount" v-on:click="updateFilter()">{{ averageRating }}</span>
+    Average Rating
+  </div>
+</template>
+
+<script>
+export default {
+  name: "average-summary",
+  methods: {
+    updateFilter() {
+      this.$store.commit("UPDATE_FILTER", 0);
+      this.$store.commit("ADD_TO_COUNTER");
+    }
+  },
+  computed: {
+    averageRating() {
+      
+      let sum = this.$store.state.reviews.reduce((currentSum, review) => {
+        return currentSum + review.rating;
+      }, 0);
+      return (sum / this.$store.state.reviews.length).toFixed(2);
+    }
+  }
+};
+</script>
