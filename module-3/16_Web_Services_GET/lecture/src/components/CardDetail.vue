f13760ec233734821d1fd8b3c8864ecd46a83d49
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-08-03 12:31:06 -0400

    Tried to follow Web Services GET

--- module-3/16_Web_Services_GET/lecture/src/components/CardDetail.vue
+++ module-3/16_Web_Services_GET/lecture/src/components/CardDetail.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <h1>{{card.title}}</h1>
+    <p>{{card.description}}</p>
+
+
+  </div>
 </template>
 
 <script>
+import BoardService from "@/services/BoardService";
+
 export default {
-  name: "card-detail"
+
+  name: "card-detail",
+  data() {
+    return {
+      card: {
+        title: '',
+        description: '',
+        status: '',
+        comments: []
+      }
+    }
+  },
+  computed() {
+    BoardService.getCard(this.$route.params.boardID, this.$route.params.cardID)
+    .then((response) => {
+      this.card = response;
+    })
+  }
 };
 </script>
