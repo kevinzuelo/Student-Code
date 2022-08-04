6a785626431b23935fc83835bb3c42f60a3cae6c
Ben Langhinrichs <benjamin@techelevator.com>
2022-08-04 12:14:53 -0400

    Exercises, lecture-final, and captions-final for M3-Day 17

---
+++ module-3/17_Web_Services_POST/captions-final/src/views/Cards.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <card-list />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardList from '@/components/CardList.vue'
+
+export default {
+  name: 'Cards',
+  components: {
+    CardList
+  }
+}
+</script>
